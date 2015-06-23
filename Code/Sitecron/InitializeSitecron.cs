7e1ac049c7b35157f65ed4e2961f68c0f4997e93
Akshay Sura <akshaysura@gmail.com>
2015-06-23 08:25:20 -0700

    added documentation and cleanup

--- Code/Sitecron/InitializeSitecron.cs
+++ Code/Sitecron/InitializeSitecron.cs
@@ -19,53 +19,6 @@ namespace Sitecron
         {
             ScheduleHelper scheduler = new ScheduleHelper();
             scheduler.InitializeScheduler();
-
-            //Log.Info("Initialize Sitecron", this);
-
-            //try
-            //{
-            //    IScheduler scheduler = StdSchedulerFactory.GetDefaultScheduler();
-            //    scheduler.Start();
-
-            //    //get a list of all items in Sitecron folder and iterate through them
-            //    //add them to the schedule
-
-            //    string queryRetriveJobs = "fast:/sitecore/system/Modules/Sitecron//*[@@templateid = '{7F2C8881-6AE4-48CF-A499-7745CC4B2EB2}']";
-            //    string typeField = "Type";
-            //    string cronExpressionField = "CronExpression";
-
-            //    scheduler.Clear();
-
-            //    Database masterDb = Factory.GetDatabase("master");
-            //    Item[] sitecronJobs = masterDb.SelectItems(queryRetriveJobs);
-            //    if (sitecronJobs != null && sitecronJobs.Count() > 0)
-            //    {
-            //        foreach (Item i in sitecronJobs)
-            //        {
-            //            if (!string.IsNullOrEmpty(i[typeField]) && !string.IsNullOrEmpty(i[cronExpressionField]))
-            //            {
-            //                Type jobType = Type.GetType(i[typeField]);
-            //                if (jobType != null)
-            //                {
-            //                    IJobDetail jobDetail = JobBuilder.Create(jobType).Build();
-
-            //                    ITrigger trigger = TriggerBuilder.Create()
-            //                        .WithIdentity(i.Name)
-            //                        .WithCronSchedule(i[cronExpressionField])
-            //                        .ForJob(jobDetail)
-            //                        .Build();
-            //                    scheduler.ScheduleJob(jobDetail, trigger);
-
-            //                    Log.Info(string.Format("Sitecron - Loading Job: {0} Type: {1} Cron Expression: {2}", i.Name, i[typeField], i[cronExpressionField]), this);
-            //                }
-            //            }
-            //        }
-            //    }
-            //}
-            //catch (Exception ex)
-            //{ 
-            //    Log.Error ("Sitecron ERROR: " + ex.Message, this);
-            //}
         }
     }
 }
--- README.txt
+++ README.txt
@@ -2,7 +2,8 @@ SiteCron module provides an advanced way to run Cron based scheduling jobs using
 
 Add your scheduled jobs at /sitecore/system/Modules/Sitecron
 
-You can get more information about the Cron triggers and examples from Quartz Scheduler website at: http://www.quartz-scheduler.net/documentation/quartz-2.x/tutorial/crontriggers.html
+You can get more information about the Cron triggers and examples from Quartz Scheduler website at: 
+http://www.quartz-scheduler.net/documentation/quartz-2.x/tutorial/crontriggers.html
 
 Sitecron comes with a sample script called SampleLogJob which logs an info log entry based on the schedule you set. Make sure any implementations of schedule jobs inherit Quartz.IJob.
 
