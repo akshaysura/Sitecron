2f5d57f7e6382d63b7b6b4ca4b986d78f8fc01d4
Mike Edwards <mike@glass.lu>
2017-08-07 13:34:46 +0100

    Updated SitecronSavedHandler to handle ItemSavedRemoteEventArgs

--- Code/Sitecron/Events/SitecronSavedHandler.cs
+++ Code/Sitecron/Events/SitecronSavedHandler.cs
@@ -2,6 +2,7 @@
 using Sitecore.Events;
 using Sitecron.SitecronSettings;
 using System;
+using Sitecore.Data.Events;
 
 namespace Sitecron.Events
 {
@@ -9,7 +10,17 @@ namespace Sitecron.Events
     {
         public void OnItemSaved(object sender, EventArgs args)
         {
-            Item savedItem = Event.ExtractParameter(args, 0) as Item;
+            Item savedItem = null;
+            ItemSavedRemoteEventArgs remoteArgs = args as ItemSavedRemoteEventArgs;
+            
+            if (remoteArgs != null)
+            {
+                savedItem = remoteArgs.Item;
+            }
+            else
+            {
+                savedItem = Event.ExtractParameter(args, 0) as Item;
+            }
 
             if (savedItem != null && SitecronConstants.Templates.SitecronJobTemplateID == savedItem.TemplateID) //matched Sitecron job template
             {
