45cf90f01e087acf2eab0381573e0ee2911bf6eb
asura <asura@webdatamation.com>
2017-08-14 07:33:12 -0700

    Cleanup

---
+++ Code/Sitecron/SitecronSettings/FieldNames.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sitecron.SitecronSettings
+{
+    public static class FieldNames
+    {
+        public const string Type = "Type";
+        public const string CronExpression = "CronExpression";
+        public const string Parameters = "Parameters";
+        public const string Items = "Items";
+    }
+}
---
+++ SampleJob/SampleJob/Parameters/TestParams.cs
@@ -0,0 +1,22 @@
+﻿using Quartz;
+using Sitecore.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleJob.Parameters
+{
+    public class TestParams : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            JobDataMap dataMap = context.JobDetail.JobDataMap;
+
+            string jobParams = dataMap.GetString("Parameters");
+
+            Log.Info(string.Format("Instance {0} of TestParams Job - {4}Parameters: {1} {4}Fired at: {2} {4}Next Scheduled For:{3}", context.JobDetail.Key, jobParams, context.FireTimeUtc.Value.ToString("r"), context.NextFireTimeUtc.Value.ToString("r"), Environment.NewLine), this);
+        }
+    }
+}
