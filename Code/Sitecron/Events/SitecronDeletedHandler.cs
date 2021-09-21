1af93c93ff2cfbfb5a777ac74342845b0bb6f4a0
JanBuehler <jnbhlr@web.de>
2021-09-21 12:44:06 +0200

    Update SitecronDeletedHandler.cs
    Copied Mike Edwards approach of parsing event Args from the OnItemSaved/Saving-Handler to the OnItemDeleted-Handler as remote events cause the same issue-24 there.

--- Code/Sitecron/Events/SitecronDeletedHandler.cs
+++ Code/Sitecron/Events/SitecronDeletedHandler.cs
@@ -1,21 +1,27 @@
-﻿using Sitecore.Data.Items;
+using Sitecore.Data.Items;
 using Sitecore.Events;
 using Sitecron.SitecronSettings;
 using System;
-
 namespace Sitecron.Events
 {
     public class SitecronDeletedHandler
     {
         public void OnItemDeleted(object sender, EventArgs args)
         {
-            Item deletedItem = Event.ExtractParameter(args, 0) as Item;
-
+			Item deletedItem = null;
+            ItemSavedRemoteEventArgs remoteArgs = args as ItemSavedRemoteEventArgs;
+            if (remoteArgs != null)
+            {
+                deletedItem = remoteArgs.Item;
+            }
+            else
+            {
+                deletedItem = Event.ExtractParameter(args, 0) as Item;
+            }
             if (deletedItem != null && SitecronConstants.Templates.SitecronJobTemplateID == deletedItem.TemplateID) //matched Sitecron job template
             {
                 ScheduleHelper scheduler = new ScheduleHelper();
                 scheduler.InitializeScheduler();
-
             }
         }
     }
